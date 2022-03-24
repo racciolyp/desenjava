b50b00c6b8b39dbdbd76676827ebff2aabdbe4ec
racciolyp <101371577+racciolyp@users.noreply.github.com>
2022-03-23 22:07:24 -0300

    Create parimpar.java

---
+++ JAVA2303/parimpar.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+public class ParImpar {
+  // atributos e propriedades
+  
+  // funcoes e procedimentos
+  public static String VerificarParImpar(int n) {
+  
+    if (n%2 ==0) {
+      return = "Par";
+    } else {
+        return "Ímpar";
+    }
+    
+  // funcao principal
+    public static void main(String[] args) {
+      Scanner teclado = new Scanner(System.in);
+      int n = teclado.nextInt();
+      
+      // codigo
+      
+      do{
+        if (n>=0 {
+          System.out.println(VerificarParImpar(n));        
+        } while (n >= 0); 
+    }
+            
+  }    
+  
+}
